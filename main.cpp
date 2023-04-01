4c331ff511fd2691346ab8690df6711dbfe4ba57
ERNANI ALEXANDRE WIPPEL NETO <e.wippelneto@gmail.com>
2023-04-01 15:10:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <fstream>
-
+#include <string>
 
 using namespace std;
 
 int main (){
-    
+    ifstream arquivo("./Exercicio1Hex");
+    string linha;
+    if(arquivo.is_open()){
+        while (getline(arquivo, linha))
+        {
+            cout << linha;   
+        }
+        
+    }
+
+
     return 0;
 }
 
